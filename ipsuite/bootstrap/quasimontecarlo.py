c750787bf52342380485a71bcf02451af9a7c5a6
Chronum94 <ahaldar@bu.edu>
2023-03-28 15:59:12 -0400

    Allow multiple point clouds and multiple configs

--- ipsuite/bootstrap/quasimontecarlo.py
+++ ipsuite/bootstrap/quasimontecarlo.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.stats.qmc import Halton
 from scipy.spatial.distance import pdist
+from ase import Atoms
 
 
 def _generate_qmc_points(cell_size, num_points, min_bond_length):
@@ -17,28 +18,38 @@ def _generate_qmc_points(cell_size, num_points, min_bond_length):
                 points_too_close.append(i)
 
     random_points_physical = np.delete(random_points_physical, points_too_close, axis=0)
+    return random_points_physical
 
 def generate_qmc_bulk_configs(
-    cell_size, species, probabilities=None, min_bond_length=2.0, bond_volume_scale=2.0, n_realizations=1
+    cell_size, 
+    species, 
+    probabilities=None, 
+    min_bond_length=2.0, 
+    bond_volume_scale=2.0, 
+    n_point_clouds=4,
+    n_realizations_per_cloud=4,
 ):
-    probabilities = np.ones(len(species)) / len(species) if None else np.array(probabilities) / np.sum(probabilities)
+    probabilities = np.ones(len(species)) / len(species) if probabilities is None else np.array(probabilities) / np.sum(probabilities)
 
-    num_points = int(scaling**3 / (bond_volume_scale * min_bond_length**3))
+    num_points = int(cell_size**3 / (bond_volume_scale * min_bond_length**3))
     
-    random_points_physical = _generate_qmc_points(cell_size, num_points, min_bond_length)
     atoms_list = []
-    for i in range(n_realizations):
-        species_array = np.random.choice(
-            species, size=len(random_points_physical), p=probabilities
-        )
-        d_x = np.ptp(random_points_physical[:, 0])
-        d_y = np.ptp(random_points_physical[:, 1])
-        d_z = np.ptp(random_points_physical[:, 2])
-        x_pad = min_bond_length - (cell_size - d_x)
-        y_pad = min_bond_length - (cell_size - d_y)
-        z_pad = min_bond_length - (cell_size - d_z)
-        cell = np.array([scaling] * 3) + np.array([x_pad, y_pad, z_pad])
-        atoms = Atoms(species_array, random_points_physical, cell=cell, pbc=True)
-        atoms.center()
-        atoms_list.append(atoms)
+    for _ in range(n_point_clouds):
+        random_points_physical = _generate_qmc_points(cell_size, num_points, min_bond_length)
+        for _ in range(n_realizations_per_cloud):
+            species_array = np.random.choice(
+                species, size=len(random_points_physical), p=probabilities
+            )
+            d_x = np.ptp(random_points_physical[:, 0])
+            d_y = np.ptp(random_points_physical[:, 1])
+            d_z = np.ptp(random_points_physical[:, 2])
+            x_pad = min_bond_length - (cell_size - d_x)
+            y_pad = min_bond_length - (cell_size - d_y)
+            z_pad = min_bond_length - (cell_size - d_z)
+            cell = np.array([cell_size] * 3) + np.array([x_pad, y_pad, z_pad])
+            atoms = Atoms(species_array, random_points_physical, cell=cell, pbc=True)
+            atoms.center()
+            atoms_list.append(atoms)
     return atoms_list
+
+print(generate_qmc_bulk_configs(13, ['C', 'B', 'N']))
